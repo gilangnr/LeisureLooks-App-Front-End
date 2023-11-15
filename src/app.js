f54d34053e62d88ad93537b506efaf5d814e7bbc
Gilang <gilangnurrizki173@gmail.com>
2023-11-15 21:45:54 +0700

    Add Feature Cart

---
+++ src/app.js
@@ -0,0 +1,70 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Corduroy Down Jacket", img: "Corduroy Down Jacket 2.jpg", price: 999000 },
+      { id: 2, name: "Long Sleeves Plaid Shirt", img: "Long Sleeves Plaid Shirt 2.jpg", price: 999000 },
+      { id: 3, name: "Loose Fit T-Shirt", img: "Loose Fit T-Shirt 2.jpg", price: 249000 },
+      { id: 4, name: "Relaxed Fit Pullover", img: "Relaxed Fit Pullover 2.jpg", price: 699000 },
+      { id: 5, name: "Ribbed Button Down Polo", img: "Ribbed Button Down Polo 2.jpg", price: 449000 },
+      { id: 6, name: "Striped T-Shirt", img: "Striped T-Shirt 2.jpeg.png", price: 249000 },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek barang yg sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+      // cart kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        this.items = this.items.map((item) => {
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      const cartItem = this.items.find((item) => item.id === id);
+
+      if (cartItem.quantity > 1) {
+        this.items = this.items.map((item) => {
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+// konversi rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
